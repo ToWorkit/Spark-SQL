8d6e48fb8a1be22061af10e99f6ef7cc47d8abba
Toworkit <1211126374@qq.com>
2018-07-06 03:42:48 +0800

    test

--- sql/src/main/scala/com/logs/spark/SchemaInferApp.scala
+++ sql/src/main/scala/com/logs/spark/SchemaInferApp.scala
@@ -9,7 +9,7 @@ object SchemaInferApp {
   def main(args: Array[String]): Unit = {
     var spark = SparkSession.builder().appName("SchemaInferApp").master("local[2]").getOrCreate()
     // 加载数据, 返回值为DataFrame格式数据
-    val df = spark.read.format("json").load("C:\\Users\\Just Do It\\Desktop\\iommc_data\\data\\json_schema.json")
+    val df = spark.read.format("json").load("C:\\Users\\Just Do It\\Desktop\\data\\imooc_spark_sql\\data\\json_schema.json")
     df.printSchema()
     df.show()
 
